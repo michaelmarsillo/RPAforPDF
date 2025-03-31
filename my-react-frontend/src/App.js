16a63b48ecbb10fdb4f2d4805de38f7ed01b9bfd
michaelmarsillo <thecaptain49@hotmail.ca>
2025-03-30 23:58:57 -0400

    Added speed insights

--- my-react-frontend/src/App.js
+++ my-react-frontend/src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import PdfForm from './PdfForm.js'; 
 import About from './About';
 import NavBar from './components/NavBar.jsx';
-// eslint-disable-next-line no-unused-vars
 import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
@@ -19,6 +18,7 @@ function App() {
           <Route path="/about" element={<About />} /> {/* About page */}
         </Routes>
       </div>
+      <SpeedInsights />
     </Router>
   );
 }
